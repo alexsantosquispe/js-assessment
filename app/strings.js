6bc9542592cc926a389d828447bf1edbfdba8e7a
Alex Santos <alexsantosquispe@gmail.com>
2025-08-04 23:16:27 -0400

    string reduce repeated strings

--- app/strings.js
+++ app/strings.js
@@ -1,7 +1,26 @@
 exports = typeof window === "undefined" ? global : window;
 
 exports.stringsAnswers = {
-  reduceString: function (str, amount) {},
+  /**
+   * Returns the string with the limited(amount) characters
+   * @param {string} str
+   * @param {number} amount
+   */
+  reduceString: function (str, amount) {
+    let result = "";
+    let count = 1;
+
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === str[i + 1]) {
+        count++;
+      } else {
+        result += str[i].repeat(Math.min(count, amount));
+        count = 1;
+      }
+    }
+
+    return result;
+  },
 
   wordWrap: function (str, cols) {},
 
