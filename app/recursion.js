3908a717e08aa47c0c11b3a4d303de24f642681c
Alex Santos <alexsantosquispe@gmail.com>
2025-08-05 21:39:44 -0400

    fibonacci

--- app/recursion.js
+++ app/recursion.js
@@ -19,7 +19,10 @@ exports.recursionAnswers = {
 
   permute: function (arr) {},
 
-  fibonacci: function (n) {},
+  fibonacci: function (n) {
+    if (n <= 1) return n;
+    return this.fibonacci(n - 1) + this.fibonacci(n - 2);
+  },
 
   validParentheses: function (n) {}
 };
